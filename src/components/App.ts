77ec87ab05ebab84b2ed272adb20e786810e39fb
waichuan <nwai1990@hotmail.com>
2020-10-01 01:16:49 +0800

    Add page to see all cards for debugging

--- src/components/App.ts
+++ src/components/App.ts
@@ -5,6 +5,7 @@ import { GamesOverview } from "./GamesOverview";
 import { PlayerHome } from "./PlayerHome";
 import { StartScreen } from "./StartScreen";
 import { LoadGameForm } from "./LoadGameForm";
+import { DebugUI } from "./DebugUI";
 
 export const mainAppSettings = {
     "el": "#app",
@@ -33,6 +34,7 @@ export const mainAppSettings = {
         "player-home": PlayerHome,
         "player-end": GameEnd,
         "games-overview": GamesOverview,
+        "debug-ui": DebugUI,
     },
     "methods": {
         setVisibilityState: function (targetVar: string, isVisible: boolean) {
@@ -123,6 +125,8 @@ export const mainAppSettings = {
             app.screen = "create-game-form";
         } else if (currentPathname === "/load") {
             app.screen = "load";
+        } else if (currentPathname === "/debug-ui") {
+            app.screen = "debug-ui";
         } else {
             app.screen = "start-screen";
         }
