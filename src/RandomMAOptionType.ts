f9b2fba7449571c00e7e2783bf28d9718ec547ce
waichuan <nwai1990@hotmail.com>
2020-10-27 11:04:10 +0800

    Minor update after rebase

--- src/RandomMAOptionType.ts
+++ src/RandomMAOptionType.ts
@@ -1,6 +1,6 @@
 
 export enum RandomMAOptionType {
     NONE = 'No randomization',
-    LIMITED = 'Limited synergy',
-    UNLIMITED = 'Full random'
+    LIMITED = 'Synergy',
+    UNLIMITED = 'Full Random'
 }
