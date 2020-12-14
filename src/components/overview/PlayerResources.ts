645a2003e30bb4af28b53db44ee5467ea8c6b94f
waichuan <nwai1990@hotmail.com>
2020-12-14 21:28:19 +0800

    Fix: Exclude Unity policy effect when indicating enhanced metals

--- src/components/overview/PlayerResources.ts
+++ src/components/overview/PlayerResources.ts
@@ -22,7 +22,7 @@ export const PlayerResources = Vue.component('player-resources', {
   template: `
         <div class="resource_items_cont">
             <player-resource :type="resources.MEGACREDITS" :count="player.megaCredits" :production="player.megaCreditProduction"></player-resource>
-            <player-resource :type="resources.STEEL" :count="player.steel" :production="player.steelProduction" :steelValue="player.steelValue"></player-resource>
+            <player-resource :type="resources.STEEL" :count="player.steel" :production="player.steelProduction" :steelValue="player.steelValue" :turmoil="player.turmoil"></player-resource>
             <player-resource :type="resources.TITANIUM" :count="player.titanium" :production="player.titaniumProduction" :titaniumValue="player.titaniumValue" :turmoil="player.turmoil"></player-resource>
             <player-resource :type="resources.PLANTS" :count="player.plants" :production="player.plantProduction" :plantsAreProtected="player.plantsAreProtected"></player-resource>
             <player-resource :type="resources.ENERGY" :count="player.energy" :production="player.energyProduction"></player-resource>
