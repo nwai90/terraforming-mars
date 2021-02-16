efcc801cbe13777e35f0914a5e404df35640ab7a
Jeep <14239220+vsrisuknimit@users.noreply.github.com>
2021-02-16 08:45:47 -0500

    Turn help-iconology to help-page

--- src/components/App.ts
+++ src/components/App.ts
@@ -8,7 +8,7 @@ import {StartScreen} from './StartScreen';
 import {LoadGameForm} from './LoadGameForm';
 import {DebugUI} from './DebugUI';
 import {GameHomeModel} from '../models/GameHomeModel';
-import {HelpIconology} from './HelpIconology';
+import {HelpPage} from './help/HelpPage';
 
 import * as constants from '../constants';
 import * as raw_settings from '../genfiles/settings.json';
@@ -19,7 +19,7 @@ interface MainAppData {
             'empty' |
             'game-home' |
             'games-overview' |
-            'help-iconology' |
+            'help-page' |
             'load' |
             'player-home' |
             'start-screen' |
@@ -67,7 +67,7 @@ export const mainAppSettings = {
     'player-end': GameEnd,
     'games-overview': GamesOverview,
     'debug-ui': DebugUI,
-    'help-iconology': HelpIconology,
+    'help-page': HelpPage,
   },
   'methods': {
     setVisibilityState: function(targetVar: string, isVisible: boolean) {
@@ -161,8 +161,8 @@ export const mainAppSettings = {
       app.screen = 'load';
     } else if (currentPathname === '/debug-ui') {
       app.screen = 'debug-ui';
-    } else if (currentPathname === '/help-iconology') {
-      app.screen = 'help-iconology';
+    } else if (currentPathname === '/help-page') {
+      app.screen = 'help-page';
     } else {
       app.screen = 'start-screen';
     }
