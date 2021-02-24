8c044623c3382c8d4364e8de4642d8a5ce3f7d50
waichuan <nwai1990@hotmail.com>
2021-02-24 10:57:07 +0800

    Fix bug for PreferentialLoans

--- src/turmoil/globalEvents/society/PreferentialLoans.ts
+++ src/turmoil/globalEvents/society/PreferentialLoans.ts
@@ -14,7 +14,7 @@ export class PreferentialLoans implements IGlobalEvent {
     public resolve(game: Game, turmoil: Turmoil) {
       game.getPlayers().forEach((player) => {
         const amount = Math.min(Math.floor((player.getTerraformRating() - 10) / 5), 5) - turmoil.getPlayerInfluence(player);
-        if (amount > -5) player.addProduction(Resources.MEGACREDITS, -amount, game, undefined, true);
+        if (amount > 0) player.addProduction(Resources.MEGACREDITS, -amount, game, undefined, true);
         player.setResource(Resources.MEGACREDITS, 10, game, undefined, true);
       });
     }
