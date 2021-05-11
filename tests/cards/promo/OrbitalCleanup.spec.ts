07cb9b33984fdf4a9158eaf65bcb51aef86de9ed
Robert Konigsberg <kberg@users.noreply.github.com>
2021-05-11 19:59:13 -0400

    Log MC gain for cards with variable MC gain. (#3278)

--- tests/cards/promo/OrbitalCleanup.spec.ts
+++ tests/cards/promo/OrbitalCleanup.spec.ts
@@ -14,7 +14,7 @@ describe('OrbitalCleanup', function() {
   beforeEach(function() {
     card = new OrbitalCleanup();
     player = TestPlayers.BLUE.newPlayer();
-    Game.newInstance('foobar', [player], player);
+    Game.newInstance('id', [player], player);
   });
 
   it('Can\'t play if cannot decrease MC production', function() {
