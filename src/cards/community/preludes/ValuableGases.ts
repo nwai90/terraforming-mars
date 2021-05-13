6ea6c0f224a1290c62058c7e2e91a8aeee0bd77f
waichuan <nwai1990@hotmail.com>
2021-05-13 19:22:36 +0800

    Update logic for Valuable Gases prelude

--- src/cards/community/preludes/ValuableGases.ts
+++ src/cards/community/preludes/ValuableGases.ts
@@ -18,10 +18,10 @@ export class ValuableGases extends PreludeCard implements IProjectCard {
         cardNumber: 'Y06',
         renderData: CardRenderer.builder((b) => {
           b.megacredits(6).br.br;
-          b.text('play', Size.MEDIUM, true).cards(1).secondaryTag(Tags.VENUS).colon();
+          b.text('play', Size.MEDIUM, true).cards(1).colon();
           b.floaters(4).digit;
         }),
-        description: 'Gain 6 M€. Play a Venus card from your hand and add 4 floaters to it.',
+        description: 'Gain 6 M€. Play a card from your hand and add 4 floaters to it.',
       },
     });
   }
@@ -29,7 +29,7 @@ export class ValuableGases extends PreludeCard implements IProjectCard {
   public play(player: Player) {
     player.megaCredits += 6;
 
-    const playableCards = player.getPlayableCards().filter((card) => card.tags.includes(Tags.VENUS));
+    const playableCards = player.getPlayableCards();
 
     if (playableCards.length > 0) {
       return new SelectHowToPayForProjectCard(
