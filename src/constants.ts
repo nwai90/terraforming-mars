8f6c136876785c7b3dc050c1aca7566825e6b848
Brian Folts <brian.folts@gmail.com>
2021-05-21 07:00:04 -0700

    add italian language (#3307)

--- src/constants.ts
+++ src/constants.ts
@@ -47,6 +47,7 @@ export const LANGUAGES = [
   {'id': 'pl', 'title': 'Polish'},
   {'id': 'es', 'title': 'Spanish'},
   {'id': 'br', 'title': 'Português Brasileiro'},
+  {'id': 'it', 'title': 'Italian'},
 ];
 
 export const APP_NAME = 'Terraforming Mars';
