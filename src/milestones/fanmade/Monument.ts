832192302d51d68ffe7268bcad69e11a8b3cade5
waichuan <nwai1990@hotmail.com>
2021-06-21 21:04:07 +0800

    Fix log order for Monument milestone

--- src/milestones/fanmade/Monument.ts
+++ src/milestones/fanmade/Monument.ts
@@ -1,3 +1,4 @@
+import {DeferredAction} from "../../deferredActions/DeferredAction";
 import {DiscardCards} from "../../deferredActions/DiscardCards";
 import {Player} from "../../Player";
 import {IMilestone} from "../IMilestone";
@@ -16,6 +17,9 @@ export class Monument implements IMilestone {
 
   public static discardCards(player: Player): void {
     player.game.defer(new DiscardCards(player, 5));
-    player.game.log('${0} discarded 5 cards', (b) => b.player(player));
+    player.game.defer(new DeferredAction(player, () => {
+      player.game.log('${0} discarded 5 cards', (b) => b.player(player));
+      return undefined;
+    }));
   }
 }
